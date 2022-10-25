9b3a6117dc27485475a5776044cd44a3224b1fff
Amaan Ahmad <27350808+amaan-ahmad@users.noreply.github.com>
2022-10-25 23:54:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+import { useState } from "react";
+import { jsPDF } from "jspdf";
+import data from "../public/data.json";
+
+export default function App() {
+  const [name, setName] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  const magic = () => {
+    const doc = new jsPDF();
+    doc.addImage(data.front, "JPEG", 5, 5, 200, 80);
+    doc.setFontSize(12);
+    doc.text(name, 26, 60);
+    doc.text("# 1111", 100, 60);
+    doc.save("ticket.pdf");
+  };
+  return (
+    <div className="App">
+      Your good name:
+      <input type="text" onChange={handleChange} value={name} /> <br />
+      <button onClick={magic}>meowwww</button>
+    </div>
+  );
+}
